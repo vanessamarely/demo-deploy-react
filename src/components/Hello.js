7b42be1fb195b2af95239259d68f17cb2e4e6291
vanessamarely <vanessamarely@gmail.com>
2022-02-18 19:08:10 -0500

    Adding commit

---
+++ src/components/Hello.js
@@ -0,0 +1,13 @@
+
+import octocat from '../assets/octocat2.svg';
+
+const Hello = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <img src={octocat} alt="Octocat" />
+    </div>
+  );
+}
+
+export default Hello;
